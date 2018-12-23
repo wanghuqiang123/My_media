3752a8f0c16f6a6736fd3a42a103a22cfcb3f5b4
wanghuqing <1608751109@qq.com>
2018-12-23 20:36:17 +0800

    2018/12/23

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,7 +22,8 @@ bool MainWindow::construct()
 {
     bool ret = true;
     ret = ret && initMenuBar();
-    ret = ret && initToolBar();
+    ret = ret && initCentralwidget();
+    ret = ret && initMeidaPlayer();
 
     return ret;
 }
@@ -34,11 +35,86 @@ bool MainWindow::initMenuBar()
 
     return ret;
 }
-bool MainWindow::initToolBar()
+bool MainWindow::initMeidaPlayer()
 {
     bool ret = true;
-    QToolBar* toolbar = addToolBar("Tool Bar");
+    player = new QMediaPlayer();
 
+    player->setVolume(m_volume->value());
+    connect(player,SIGNAL(stateChanged(QMediaPlayer::State state)),this,SLOT(StateChanged(QMediaPlayer::State)));
+    connect(player,SIGNAL(positionChanged(qint64)),this,SLOT(PositionChanged(qint64)));
+    player->setMedia(QUrl::fromLocalFile("C:\\Qt\\QT_file\\Music_Player\\Qt_template\\test_misic\\1.mp3"));
+    player->play();
+    return ret;
+}
+bool MainWindow::initCentralwidget()
+{
+    bool ret =true;
+    setCentralWidget(new QWidget());
+    VLayout = new QVBoxLayout(centralWidget());
+
+    ret = ret && initProcessSlider();
+    ret = ret && initControlButton();
+
+    return ret;
+}
+bool MainWindow::initProcessSlider()
+{
+    bool ret = true;
+    QHBoxLayout* HLyout = new QHBoxLayout();
+    QSlider* process_slider = new QSlider(Qt::Horizontal);
+    HLyout->addWidget(process_slider);
+    QLabel* label = new QLabel("00:00:00");
+    HLyout->addWidget(label);
+    if(label == NULL || HLyout == NULL || process_slider == NULL)
+    {
+        ret = false;
+    }
+    else
+    {
+        VLayout->addStretch(4);
+        VLayout->addLayout(HLyout);
+    }
+    return ret;
+}
+bool MainWindow::initControlButton()
+{
+    bool ret = true;
+    QHBoxLayout* layout = new QHBoxLayout();
+    ret = (layout != NULL);
+    if(ret)
+    {
+        QPushButton* btn = NULL;
+        ret = ret && makeControlBtn(btn,layout,":/btnIcon/Icon/stop.png",Qt::Key_Stop);
+        if(ret)
+        {
+        }
+        ret = ret && makeControlBtn(btn,layout,":/btnIcon/Icon/play.png",Qt::Key_Play);
+        if(ret)
+        {}
+        ret = ret && makeControlBtn(btn,layout,":/btnIcon/Icon/pause.png",Qt::Key_Pause);
+        if(ret)
+        {
+        }
+        layout->addSpacing(30);
+        QLabel* label = new QLabel("音量：");
+        layout->addWidget(label);
+        ret = ret && makeControlBtn(btn,layout,":/btnIcon/Icon/volume.png",0);
+        if(ret)
+        {
+        }
+        ret = ret && initVolumeSlider(layout);
+        layout->addStretch(1);
+    }
+    if(ret)
+    {
+        VLayout->addLayout(layout);
+    }
+    else
+    {
+        delete layout;
+        ret = false;
+    }
     return ret;
 }
 bool MainWindow::initFileMenu(QMenuBar* mb)
@@ -64,7 +140,6 @@ bool MainWindow::initFileMenu(QMenuBar* mb)
     {
         delete menu;
     }
-
     return ret;
 }
 bool MainWindow::makeAction(QAction*& action,QWidget* parent, QString text, int key)
@@ -84,7 +159,69 @@ bool MainWindow::makeAction(QAction*& action,QWidget* parent, QString text, int 
 
     return ret;
 }
+bool MainWindow::makeControlBtn(QPushButton*& btn,QHBoxLayout* layout,QString icon,int key)
+{
+    bool ret = true;
+    btn = new QPushButton();
+    if(btn != NULL)
+    {
+        btn->setIcon(QIcon(icon));
+        btn->setShortcut(QKeySequence(key));
+        layout->addWidget(btn);
+        btn_cont.push_back(btn);
+    }
+    else
+    {
+        delete btn;
+        ret = false;
+    }
+    return ret;
+}
+bool MainWindow::initVolumeSlider(QHBoxLayout*& layout)
+{
+    bool ret = true;
+    QSlider* m_volume = new QSlider(Qt::Horizontal);
+    if(m_volume != NULL)
+    {
+        m_volume->setMinimum(0);
+        m_volume->setMaximum(100);
+        m_volume->setSliderPosition(15);
+        layout->addWidget(m_volume);
+    }
+    else
+    {
+        delete m_volume;
+        ret = false;
+    }
+    return ret;
+}
+void MainWindow::StateChanged(QMediaPlayer::State state)  //当前播放器的状态
+{
+    if(state == QMediaPlayer::PlayingState)
+    {
+        btn_cont[QMediaPlayer::PlayingState]->setEnabled(false);   //当在播放的时候 播放按钮不能使能。
+        btn_cont[QMediaPlayer::PausedState]->setEnabled(true);
+        btn_cont[QMediaPlayer::StoppedState]->setEnabled(true);
+    }
+    else if(state == QMediaPlayer::PausedState)
+    {
+        btn_cont[QMediaPlayer::PausedState]->setEnabled(false);
+        btn_cont[QMediaPlayer::StoppedState]->setEnabled(true);
+        btn_cont[QMediaPlayer::PlayingState]->setEnabled(true);
+    }
+    else
+    {
+        btn_cont[QMediaPlayer::StoppedState]->setEnabled(false);
+        btn_cont[QMediaPlayer::PlayingState]->setEnabled(true);
+        btn_cont[QMediaPlayer::PausedState]->setEnabled(false);
+    }
+}
+void MainWindow::PositionChanged(qint64 position)
+{
+
+}
 MainWindow::~MainWindow()
 {
 
 }
+
--- mainwindow.h
+++ mainwindow.h
@@ -3,10 +3,26 @@
 
 #include <QMainWindow>
 #include <QMenu>
+#include <QIcon>
+#include <QPushButton>
 #include <QMenuBar>
 #include <QToolBar>
 #include <QSize>
-
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QSlider>
+#include <QLabel>
+#include <QPixmap>
+#include <QFileDialog>
+#include <QMediaPlayer>
+#include <QMediaMetaData>
+#include <QTime>
+#include <QDebug>
+#include <QVector>
+#include <QString>
+#include <QAbstractSlider>
+#include <QMediaContent>
+#include <QUrl>
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -20,11 +36,26 @@ protected:
     MainWindow(QWidget *parent = 0);
 
     bool initMenuBar();
-    bool initToolBar();
+    bool initCentralwidget();
+    bool initProcessSlider();
+    bool initControlButton();
+    bool initVolumeSlider(QHBoxLayout*&);
 
     bool initFileMenu(QMenuBar*);
+
+    bool initMeidaPlayer();
+protected slots:
+    void StateChanged(QMediaPlayer::State state);  //当前播放器的状态
+    void PositionChanged(qint64 position);
 private:
+    QVBoxLayout* VLayout;
+    QMediaPlayer* player;
+    QSlider* m_volume;
+    QVector<QPushButton*>btn_cont;
+
     bool makeAction(QAction*& ,QWidget* , QString , int );
+    bool makeControlBtn(QPushButton*&,QHBoxLayout*,QString,int);
+
 };
 
 #endif // MAINWINDOW_H
